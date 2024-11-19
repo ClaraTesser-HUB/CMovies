84d6b4585e2af77ea3534930ffa8bfb3cc7623b8
ClaraTesser-HUB <claratesserpereira@gmail.com>
2024-11-19 16:48:57 -0300

    Começo do projeto

---
+++ site.js
@@ -0,0 +1,43 @@
+//Evento de scroll da pag
+window.addEventListener('scroll', function(){
+    const navbar = document.querySelector('.navbar');
+    if (this.window.scrollY > 50)
+        navbar.classList.add('scrolled')
+    else
+    navbar.classList.removed('scrolled')
+})
+
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YmNlMzA4MDA5MTM4YzExYmQ1ZWMxNTA4NmY2ODQ4MyIsIm5iZiI6MTczMjA0MTEyNy43OTMzNzI2LCJzdWIiOiI2MGMwMGYzMDViMTI0MDAwN2E3NDdiMjQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.7oBmC6vtiAsSMV6Jz0D4Jt_SMiYd8LrnRF39BCVUnSg'
+    }
+  };
+
+  document.addEventListener("DOMContentLoaded", async () => {
+    await banner();
+  })
+
+  async function banner() {
+    let trendings =[];
+    await fetch('https://api.themoviedb.org/3/trending/movie/day?language=pt-br', options)
+    .then(res => res.json())
+    .then(res=>res.results.forEach(movie=>{
+        trendings.push(movie)
+    }))
+    .catch(err=>console.error(err));
+    let carousel = document.querySelector('.carousel-inner');
+    carousel.innerHTML='';
+    for (let i = 0; i < trendings.length; i++) {
+        let active = i ==0? 'active': '';
+        carousel.innerHTML +=
+          `<div class="carousel-item ${active}">
+            <img src="https://image.tmdb.org/t/p/original/${trendings[i].backdrop_path}" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+              <h3>${trendings[i].title}</h3>
+              <p>${trendings[i].overview}</p>
+            </div>`;
+        
+    }
+  }
